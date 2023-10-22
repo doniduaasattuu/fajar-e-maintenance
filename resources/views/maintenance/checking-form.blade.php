071206a26d12c06aa2fdfe8abda28054d51523ec
Doni Darmawan <doni.duaasattuu@gmail.com>
2023-10-22 21:44:04 +0700

    create backup database PM3 seeded

--- resources/views/maintenance/checking-form.blade.php
+++ resources/views/maintenance/checking-form.blade.php
@@ -14,6 +14,16 @@
         </div>
         @endisset
 
+        <a href="/trends/{{ $emo->id }}" title="Record temperature and vibration in one month">
+            <button class="btn btn-success fw-bold mb-2 text-white">
+                <svg class="mb-1 me-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-graph-up" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07Z" />
+                </svg>
+                TRENDS
+            </button>
+        </a>
+
+
         <!-- MOTOR DETAILS -->
         <div class="accordion mb-4" id="accordionExample">
             <div class="accordion-item">
@@ -63,7 +73,6 @@
                 </div>
             </div>
         </div>
-
 
         <!-- CHECKING -->
         <form action="/checking-form" method="post">
@@ -239,11 +248,6 @@
 
         const weight = document.getElementById("weight");
         changeUnit(weight, "Kg");
-
-        // CHANGE NAVBAR HREF
-        $navbar_trends = document.getElementById("navbar_trends");
-        $navbar_trends.removeAttribute("href");
-        $navbar_trends.setAttribute("href", "/trends/{{ $emo->id }}");
     </script>
 </body>
 
